fd412bee1e415cb26fb10e79f23c8f2e21765fa4
Paguta <pagutaad@students.nu-lipa.edu.ph>
2025-05-15 11:05:02 +0800

    second commit

--- classes/database.php
+++ classes/database.php
@@ -1,34 +1,39 @@
 <?php
-
-class database{
-
-    function opencon(){
+ 
+class database {
+ 
+    function opencon() {
         return new PDO(
-            'mysql:host=localhost; dbname=dbs_app',
-            username: 'root',
-            password: '');
-        }
-    
-    
-    function signupUser($firstname, $lastname, $username, $password){
+            'mysql:host=localhost;dbname=dbs_app',
+            'root',
+            ''
+        );
+    }
+ 
+    function signupUser($username, $password, $firstname, $lastname) {
         $con = $this->opencon();
-
-
-        try{
+ 
+        try {
             $con->beginTransaction();
-            $stmt= $con->prepare("INSERT INTO Admin (admin_FN, admin_LN, 
-            admin_username, admin_password)VALUES (?,?,?,?)");
+ 
+            $stmt = $con->prepare("INSERT INTO Admin (admin_FN, admin_LN, admin_username, admin_password) VALUES (?, ?, ?, ?)");
             $stmt->execute([$firstname, $lastname, $username, $password]);
+ 
             $userId = $con->lastInsertId();
             $con->commit();
+ 
             return $userId;
-        }catch (PDOException $e){
+        } catch (PDOException $e) {
             $con->rollBack();
             return false;
         }
     }
-
+    function isUsernameExists ($username) {
+        $con = $this->opencon();
+        $stmt = $con->prepare("SELECT COUNT(*) FROM Admin WHERE admin_username = ?");
+        $stmt->execute([$username]);
+        $count = $stmt->fetchColumn();
+        return $count > 0;
+        }
 }
-  
- 
  
